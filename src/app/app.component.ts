d2a5f55787cdf98c4dc97bb060f18843c5830051
Osnelery <andy35223@gmail.com>
2025-03-17 21:59:53 +0800

    大約切版

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,20 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { View01Component } from './components/view01/view01.component';
+import { SidebarModule } from '@coreui/angular';
 
 @Component({
   selector: 'app-root',
-  imports: [MatButtonModule, MatSidenavModule],
+  imports: [
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    View01Component,
+    SidebarModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
