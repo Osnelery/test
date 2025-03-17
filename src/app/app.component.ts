df641f017abc71bc5e3e356ca3328b7592396c93
Osnelery <andy35223@gmail.com>
2025-03-17 22:37:40 +0800

    修改UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { View01Component } from "./components/view01/view01.component";
 
 @Component({
   selector: 'app-root',
-  imports: [MatButtonModule, MatSidenavModule],
+  imports: [MatButtonModule, MatSidenavModule, MatToolbarModule, MatIconModule, View01Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
