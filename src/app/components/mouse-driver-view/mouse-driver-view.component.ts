d1c9f4e9fc0d5c6cd3d9e8688a9c8f91e8d16f4c
Osnelery <andy35223@gmail.com>
2025-03-19 22:59:03 +0800

    新增多個

--- src/app/components/mouse-driver-view/mouse-driver-view.component.ts
+++ src/app/components/mouse-driver-view/mouse-driver-view.component.ts
@@ -12,8 +12,34 @@ export class MouseDriverViewComponent {
 
   data = [
     {
+      id: '1',
       src: 'images/Lamzu.jpg',
       title: 'Lamzu 蘭族',
+    },
+    {
+      id: '2',
+      src: 'images/Ninjutso.jpg',
+      title: 'Ninjutso',
+    },
+    {
+      id: '3',
+      src: 'images/Ajazz.jpg',
+      title: 'Ajazz 黑爵',
+    },
+    {
+      id: '4',
+      src: 'images/Bloody Gaming.png',
+      title: 'Bloody Gaming 血手幽靈',
+    },
+    {
+      id: '5',
+      src: 'images/Cooler Master.jpg',
+      title: 'CoolerMaster 酷碼',
+    },
+    {
+      id: '6',
+      src: 'images/Corsair.png',
+      title: 'Corsair 海盜船',
     },
   ];
 }
