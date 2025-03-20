4aff9c6e05ab85eb91b62f3bdca9570dbd4e3e96
Osnelery <andy35223@gmail.com>
2025-03-20 21:18:00 +0800

    新增新增一堆牌子

--- src/app/components/mouse-driver-view/mouse-driver-view.component.ts
+++ src/app/components/mouse-driver-view/mouse-driver-view.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { data } from './model/data';
 
 @Component({
   selector: 'app-mouse-driver-view',
@@ -10,36 +11,5 @@ import { Component } from '@angular/core';
 export class MouseDriverViewComponent {
   constructor() {}
 
-  data = [
-    {
-      id: '1',
-      src: 'images/Lamzu.jpg',
-      title: 'Lamzu 蘭族',
-    },
-    {
-      id: '2',
-      src: 'images/Ninjutso.jpg',
-      title: 'Ninjutso',
-    },
-    {
-      id: '3',
-      src: 'images/Ajazz.jpg',
-      title: 'Ajazz 黑爵',
-    },
-    {
-      id: '4',
-      src: 'images/Bloody Gaming.png',
-      title: 'Bloody Gaming 血手幽靈',
-    },
-    {
-      id: '5',
-      src: 'images/Cooler Master.jpg',
-      title: 'CoolerMaster 酷碼',
-    },
-    {
-      id: '6',
-      src: 'images/Corsair.png',
-      title: 'Corsair 海盜船',
-    },
-  ];
+  driverBagData = data
 }
