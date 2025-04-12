2583be0f2b43a853ef1e1b3b1df2f125d2dfac09
Osnelery <158756388+Osnelery@users.noreply.github.com>
2025-04-12 21:21:47 +0800

    qweweq

--- src/app/views/brand-overview/brand-overview.component.ts
+++ src/app/views/brand-overview/brand-overview.component.ts
@@ -4,18 +4,27 @@ import { MatChipsModule } from '@angular/material/chips';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { dataList } from './data/data';
+import { SortByNamePipePipe } from '../../pipes/sort-by-name-pipe.pipe';
 
 @Component({
   selector: 'app-brand-overview',
-  imports: [MatChipsModule, CommonModule, MatCardModule, MatButtonModule],
+  imports: [
+    MatChipsModule,
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    SortByNamePipePipe,
+  ],
   templateUrl: './brand-overview.component.html',
   styleUrl: './brand-overview.component.scss',
 })
 export class BrandOverviewComponent {
+  constructor() {}
   typeTotal: string[] = [
     '滑鼠',
     '鍵盤',
     '滑鼠墊',
+    '客製化滑鼠墊',
     '防滑貼',
     '腳貼',
     '改裝配件',
@@ -24,9 +33,23 @@ export class BrandOverviewComponent {
   ];
 
   data = dataList;
-  allSize: number = 0;
+  tagList: string[] = [];
 
-  ngOnInit() {
-    this.allSize = this.data.length;
+  ngOnInit() {}
+
+  get dataSize(): number {
+    return this.data.length;
+  }
+
+  tagClick(tag: string): void {
+    const index = this.tagList.indexOf(tag);
+
+    if (index === -1) {
+      this.tagList.push(tag);
+    } else {
+      this.tagList.splice(index, 1);
+    }
+
+    console.log(this.tagList);
   }
 }
